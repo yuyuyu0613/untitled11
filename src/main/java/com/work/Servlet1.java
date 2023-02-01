6b75b2665503b53421144df5ecf00e28cce4a8f0
ayuyu <ayuyuwoai@qq.com>
2023-02-01 21:49:21 +0800

    first commit

---
+++ src/main/java/com/work/Servlet1.java
@@ -0,0 +1,24 @@
+package com.work;
+
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@Controller
+public class Servlet1 {
+
+
+@RequestMapping("/abc")
+public String index(){
+    System.out.println("ing....");
+    return "test2";
+}
+
+
+}
+
